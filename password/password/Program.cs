ecdbe57d8ddf2edca647935ab298105a85049263
Aurélie Besnard <aurelie.besnard@aum.bio>
2022-02-17 20:13:31 +0100

    ajout d'un module

--- password/password/Program.cs
+++ password/password/Program.cs
@@ -1,46 +1,15 @@
-﻿using System;
+﻿using password;
+using System;
 
 namespace Generateur_mot_de_passe
 {
     internal class Program
     {
-        static int DemanderNombrePositifNonNul(string question)
-        {
-            return DemanderNombreEntre(question, 1, int.MaxValue);
-        }
-        static int DemanderNombreEntre(string question, int min, int max)
-        {
-
-            int nombre = DemanderNombre(question);
-            if ((nombre >= min) || (nombre <= max))
-            {
-                return nombre;
-            };
-            Console.WriteLine("le nombre doit être compris entre : " + min + "et : " + max);
-            return DemanderNombreEntre(question, min, max);
-
-        }
-        static int DemanderNombre(string question)
-        {
-            while (true)
-            {
-                Console.Write(question);
-                string reponse = Console.ReadLine();
-                try
-                {
-                    int reponseInt = int.Parse(reponse);
-                    return reponseInt;
-                }
-                catch
-                {
-                    Console.WriteLine("Vous devez rentrer un nombre");
-                }
-            }
-        }
+       
         static void Main(string[] args)
         {
 
-            int longueurMotPasse = DemanderNombre("quelle longueur de mot de passe ?");
+            int longueurMotPasse = outil.DemanderNombrePositifNonNul("quelle longueur de mot de passe ?");
         }
     }
 }
---
+++ password/password/outil.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace password
+{
+    static class outil
+    {
+        public static int DemanderNombrePositifNonNul(string question)
+        {
+            return DemanderNombreEntre(question, 1, int.MaxValue);
+        }
+        static int DemanderNombreEntre(string question, int min, int max)
+        {
+
+            int nombre = DemanderNombre(question);
+            if ((nombre >= min) || (nombre <= max))
+            {
+                return nombre;
+            };
+            Console.WriteLine("le nombre doit être compris entre : " + min + "et : " + max);
+            return DemanderNombreEntre(question, min, max);
+
+        }
+        static int DemanderNombre(string question)
+        {
+            while (true)
+            {
+                Console.Write(question);
+                string reponse = Console.ReadLine();
+                try
+                {
+                    int reponseInt = int.Parse(reponse);
+                    return reponseInt;
+                }
+                catch
+                {
+                    Console.WriteLine("Vous devez rentrer un nombre");
+                }
+            }
+        }
+    }
+}
