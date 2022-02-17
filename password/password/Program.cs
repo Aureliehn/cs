b7b33766127c2bfdb8a2c85ade664e2b005d7508
Aurélie Besnard <aurelie.besnard@aum.bio>
2022-02-17 20:59:07 +0100

    création des mdp

--- password/password/Program.cs
+++ password/password/Program.cs
@@ -8,23 +8,56 @@ namespace Generateur_mot_de_passe
        
         static void Main(string[] args)
         {
-
+            const int NBR_MDP = 10;
             int longueurMotPasse = outil.DemanderNombrePositifNonNul("quelle longueur de mot de passe ?");
+            Console.WriteLine();
+            int choixAlphabet = outil.DemanderNombreEntre("Vous voulez un mot de passe avec :\n" +
+                "1 : uniquement des minuscules\n" +
+                "2 : des minuscules et des majuscules\n" +
+                "3 : des lettres et des chiffres\n"+
+                "4 : des lettres des chiffres et des caractères spéciaux\n"+
+                "Votre choix : "
+                , 1, 4);
+            
+            
             string minuscules = "abcdefghijklmnopqrstuvwxyz";
-            string alphabet = minuscules;
+            string majuscules = minuscules.ToUpper();
+            string chiffres = "1234567890";
+            string caracteresSpeciaux = "#%-/";
+            string alphabet;
             string motDePasse = "";
-            int l =alphabet.Length;
+            
             Random random = new Random();
 
-            for(int i=0;i<longueurMotPasse; i++)
+            if(choixAlphabet == 1)
             {
-                int index = random.Next(0, l);
-                //Console.WriteLine(alphabet[index]);
-                motDePasse += alphabet[index];
+                alphabet = minuscules;
             }
-            
+            else if (choixAlphabet == 2)
+            {
+                alphabet=minuscules + majuscules;
+            }
+            else if (choixAlphabet == 3)
+            {
+                alphabet = minuscules+chiffres+majuscules;
+            }
+            else
+            {
+                alphabet = minuscules+chiffres+majuscules+caracteresSpeciaux;
+            }
+            int l = alphabet.Length;
 
-            Console.WriteLine("mot de passe : " + motDePasse);
+            for(int j = 0; j < NBR_MDP; j++)
+            {
+                motDePasse = "";
+                for (int i = 0; i < longueurMotPasse; i++)
+                {
+                    int index = random.Next(0, l);
+                    motDePasse += alphabet[index];
+                }
+                Console.WriteLine("mot de passe : " + motDePasse);
+            }
+         
         }
     }
 }
